eae93fa4dbda906d0355eca4c33db8cd1c2897b9
emmanuelist <emmanuel.paul75@yahoo.com>
2024-09-30 16:23:22 +0100

    Add test for withdrawing STX

--- tests/lending-pool.test.ts
+++ tests/lending-pool.test.ts
@@ -31,4 +31,29 @@ describe("lending-pool", () => {
     );
     expect(balance.result).toBeOk(Cl.uint(amount));
   });
+
+  it("allows users to withdraw STX", () => {
+    const depositAmount = 1000000;
+    const withdrawAmount = 500000;
+    simnet.callPublicFn(
+      "lending-pool",
+      "deposit",
+      [Cl.uint(depositAmount)],
+      wallet1
+    );
+    const withdraw = simnet.callPublicFn(
+      "lending-pool",
+      "withdraw",
+      [Cl.uint(withdrawAmount)],
+      wallet1
+    );
+    expect(withdraw.result).toBeOk(Cl.uint(withdrawAmount));
+    const balance = simnet.callReadOnlyFn(
+      "lending-pool",
+      "get-deposit",
+      [Cl.principal(wallet1)],
+      wallet1
+    );
+    expect(balance.result).toBeOk(Cl.uint(depositAmount - withdrawAmount));
+  });
 });
